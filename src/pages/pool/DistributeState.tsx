a27f81519ba3ec17cf5d6366934975540718211e
socemtzmdrmndfg <lxhyl6@gmail.com>
2022-09-26 16:41:01 +0800

    chore: add distributed claimed status

--- src/pages/pool/DistributeState.tsx
+++ src/pages/pool/DistributeState.tsx
@@ -24,31 +24,38 @@ export function DistributeState({
   distributeEvent?: ActionEvent
 }) {
   const { state, deadline, claimedAmount, totalAmount } = poolMeta
-  console.log('poolMeta', poolMeta)
   const colorClass = useMemo(() => stateColorMap[state], [state])
+
   if (
     distributeEvent ||
     (deadline === 2 ** 48 - 1 && claimedAmount.eq(totalAmount))
   ) {
     return <div className="flex ml-2 text-gray-500 text-xs">Distributed</div>
   }
+  if (state === PoolState.Closed && claimedAmount.eq(totalAmount)) {
+    return <div className="flex ml-2 text-gray-500 text-xs">Claimed</div>
+  }
+  if (state === PoolState.Initialized) {
+    return <div className="flex ml-2 text-gray-500 text-xs">Initialized</div>
+  }
+  if (state === PoolState.Closed) {
+    return <div className="flex ml-2 text-red-500 text-xs">Closed</div>
+  }
   return (
     <div className="flex">
-      <div className="ml-2 text-gray-500 text-xs flex items-center">
-        {stateMsg[state]}
+      <div className="ml-2 text-green-500 text-xs flex items-center">
+        Funded
       </div>
-      {state === PoolState.Funded && (
-        <div className="relative">
-          <div
-            className={`w-2 h-2 rounded-full cursor-pointer`}
-            style={{ background: colorClass }}
-          ></div>
-          <div
-            className="w-2 h-2 absolute left-0 top-0 rounded-full cursor-pointer animate-ping "
-            style={{ background: colorClass }}
-          ></div>
-        </div>
-      )}
+      <div className="relative">
+        <div
+          className={`w-2 h-2 rounded-full cursor-pointer`}
+          style={{ background: colorClass }}
+        ></div>
+        <div
+          className="w-2 h-2 absolute left-0 top-0 rounded-full cursor-pointer animate-ping "
+          style={{ background: colorClass }}
+        ></div>
+      </div>
     </div>
   )
 }
